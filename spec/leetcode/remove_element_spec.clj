81f7746c93fe729f9dd8800f704c45165efeb5fa
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-12-07 18:09:33 +0800

    add more testcase
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- spec/leetcode/remove_element_spec.clj
+++ spec/leetcode/remove_element_spec.clj
@@ -12,15 +12,12 @@
   The order of elements can be changed. It doesn't matter what you leave beyond
   the new length."
 
- (verify {:nums [1 2 3 4 5]
-          :val 5
-          :expect [1 2 3 4]})
+ ;; testcase I wrote
+ (verify {:nums [1 2 3 4 5] :val 5 :expect [1 2 3 4]})
+ (verify {:nums [1 2 3 4]   :val 3 :expect [1 2 4]})
+ (verify {:nums [1 2 3 4]   :val 5 :expect [1 2 3 4]})
 
- (verify {:nums [1 2 3 4]
-          :val 3
-          :expect [1 2 4]})
-
- (verify {:nums [1 2 3 4]
-          :val 5
-          :expect [1 2 3 4]})
+ ;; https://github.com/samfang/leetcode/blob/master/26.%20Remove%20Element
+ (verify {:nums [2]         :val 3 :expect [2]})
+ (verify {:nums [4 5]       :val 4 :expect [5]})
  )
